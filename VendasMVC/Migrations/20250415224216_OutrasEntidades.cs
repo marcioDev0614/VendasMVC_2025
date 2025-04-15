13cb0cdbbdea2d7966c18cd765b549be0507976d
Marcio <marciodev0614@gmail.com>
2025-04-15 19:45:30 -0300

    Criacao das classes vendedor vendas e migration

---
+++ VendasMVC/Migrations/20250415224216_OutrasEntidades.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VendasMVC.Migrations
+{
+    public partial class OutrasEntidades : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vendedor",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DataAniversario = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SalarioBase = table.Column<double>(type: "float", nullable: false),
+                    DepartamentoId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vendedor", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Vendedor_Departamento_DepartamentoId",
+                        column: x => x.DepartamentoId,
+                        principalTable: "Departamento",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Venda",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Data = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Quantia = table.Column<double>(type: "float", nullable: false),
+                    Situacao = table.Column<int>(type: "int", nullable: false),
+                    VendedorId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Venda", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Venda_Vendedor_VendedorId",
+                        column: x => x.VendedorId,
+                        principalTable: "Vendedor",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Venda_VendedorId",
+                table: "Venda",
+                column: "VendedorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vendedor_DepartamentoId",
+                table: "Vendedor",
+                column: "DepartamentoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Venda");
+
+            migrationBuilder.DropTable(
+                name: "Vendedor");
+        }
+    }
+}
--- VendasMVC/Models/Departamento.cs
+++ VendasMVC/Models/Departamento.cs
@@ -1,8 +1,33 @@
-﻿namespace VendasMVC.Models
+﻿using VendasMVC.Models;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace VendasMVC.Models
+
 {
     public class Departamento
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+        public ICollection<Vendedor> Vendedores { get; set; } = new List<Vendedor>();
+
+        public Departamento() { }
+
+        public Departamento(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+
+        public void AdicionarVendedor(Vendedor vendedor)
+        {
+            Vendedores.Add(vendedor);
+        }
+
+        public double TotalVendas(DateTime inicial, DateTime final)
+        {
+            return Vendedores.Sum(vendedor => vendedor.TotalVendas(inicial, final));
+        }
     }
 }
