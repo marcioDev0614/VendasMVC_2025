a80166c6086d7816261de121855fee130e7fdfff
Marcio <marciodev0614@gmail.com>
2025-04-22 20:31:54 -0300

    Criação da foreignKey DepartamentoID

---
+++ VendasMVC/Controllers/VendedoresController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VendasMVC.Models;
+using VendasMVC.Data;
+using VendasMVC.Services;
+
+namespace VendasMVC.Controllers
+{
+    public class VendedoresController : Controller
+    {
+
+        private readonly VendedorServico _vendedorServico;
+
+        public VendedoresController(VendedorServico vendedorServico)
+        {
+            _vendedorServico = vendedorServico;
+        }
+
+        public IActionResult Index()
+        {
+
+            var list = _vendedorServico.BuscarTodos();
+
+            return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Vendedor vendedor)
+        {
+           _vendedorServico.Insert(vendedor);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ VendasMVC/Migrations/20250422232154_DepartamentoForeignKey.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VendasMVC.Migrations
+{
+    public partial class DepartamentoForeignKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vendedor_Departamento_DepartamentoId",
+                table: "Vendedor");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartamentoId",
+                table: "Vendedor",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vendedor_Departamento_DepartamentoId",
+                table: "Vendedor",
+                column: "DepartamentoId",
+                principalTable: "Departamento",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vendedor_Departamento_DepartamentoId",
+                table: "Vendedor");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartamentoId",
+                table: "Vendedor",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vendedor_Departamento_DepartamentoId",
+                table: "Vendedor",
+                column: "DepartamentoId",
+                principalTable: "Departamento",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
